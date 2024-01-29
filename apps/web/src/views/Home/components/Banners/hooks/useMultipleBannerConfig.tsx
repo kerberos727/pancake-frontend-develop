5ba79f1334cabec030e5d065b8f4425b2d89eaf5
kerberos727 <abe.yusei727@gmail.com>
2024-01-29 22:00:25 +0900

    update

---
+++ apps/web/src/views/Home/components/Banners/hooks/useMultipleBannerConfig.tsx
@@ -0,0 +1,76 @@
+import shuffle from 'lodash/shuffle'
+import { ReactElement, useMemo } from 'react'
+import CompetitionBanner from '../CompetitionBanner'
+import GameBanner from '../GameBanner'
+import IFOBanner from '../IFOBanner'
+import PerpetualBanner from '../PerpetualBanner'
+import { SpainMeetupBanner } from '../SpainMeetupBanner'
+import UserBanner from '../UserBanner'
+import VeCakeBanner from '../VeCakeBanner'
+import WebNotificationBanner from '../WebNotificationBanner'
+import useIsRenderCompetitionBanner from './useIsRenderCompetitionBanner'
+import useIsRenderIfoBanner from './useIsRenderIFOBanner'
+import useIsRenderUserBanner from './useIsRenderUserBanner'
+
+interface IBannerConfig {
+  shouldRender: boolean
+  banner: ReactElement
+}
+
+/**
+ * make your custom hook to control should render specific banner or not
+ * add new campaign banner easily
+ *
+ * @example
+ * ```ts
+ *  {
+ *    shouldRender: isRenderIFOBanner,
+ *    banner: <IFOBanner />,
+ *  },
+ * ```
+ */
+
+export const useMultipleBannerConfig = () => {
+  const isRenderIFOBanner = useIsRenderIfoBanner()
+  const isRenderCompetitionBanner = useIsRenderCompetitionBanner()
+  const isRenderUserBanner = useIsRenderUserBanner()
+
+  return useMemo(() => {
+    const NO_SHUFFLE_BANNERS: IBannerConfig[] = [
+      { shouldRender: true, banner: <SpainMeetupBanner /> },
+      { shouldRender: true, banner: <WebNotificationBanner /> },
+      { shouldRender: true, banner: <VeCakeBanner /> },
+      { shouldRender: true, banner: <GameBanner /> },
+      {
+        shouldRender: isRenderUserBanner.shouldRender && !isRenderUserBanner.isEarningsBusdZero,
+        banner: <UserBanner />,
+      },
+      {
+        shouldRender: isRenderIFOBanner,
+        banner: <IFOBanner />,
+      },
+    ]
+
+    const SHUFFLE_BANNERS: IBannerConfig[] = [
+      {
+        shouldRender: isRenderCompetitionBanner,
+        banner: <CompetitionBanner />,
+      },
+      {
+        shouldRender: true,
+        banner: <PerpetualBanner />,
+      },
+    ]
+    return [
+      ...NO_SHUFFLE_BANNERS,
+      ...shuffle(SHUFFLE_BANNERS),
+      {
+        // be the last one if harvest value is zero
+        shouldRender: isRenderUserBanner.shouldRender && isRenderUserBanner.isEarningsBusdZero,
+        banner: <UserBanner />,
+      },
+    ]
+      .filter((bannerConfig: IBannerConfig) => bannerConfig.shouldRender)
+      .map((bannerConfig: IBannerConfig) => bannerConfig.banner)
+  }, [isRenderIFOBanner, isRenderCompetitionBanner, isRenderUserBanner])
+}
